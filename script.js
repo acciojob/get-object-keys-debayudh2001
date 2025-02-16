f93be23acde9e24b3278330195cdc60d0d7153fe
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-16 10:42:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 let student = {
 	name: "Dev",
-	getKeys: () => {
-		return Object.keys(student)
-	}
+}
+
+student.__proto__.getKeys = () => {
+	return Object.keys(student)
 }
 
 console.log(student.getKeys())
