31c82eb7d675452ff3d73863fac7aaaeeb6ac9c6
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-16 09:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
 let student = {
-	name: "Dev"
+	name: "Dev",
+	age: 22,
+	gender: "male"
 }
 
 function getKeys(obj){
 	return Object.keys(obj)
 }
+
+
 
 console.log(getKeys(student))
